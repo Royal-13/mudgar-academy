f9e1f98cbb4799f6d2b81451f32e96cef8c49f2c
Karan Kartikeya <karankartikey72@gmail.com>
2023-10-11 00:41:09 +0530

    dev branch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
-import '../public/css/style.css';
-import '../public/css/bootstrap.css';
+import '../styles/css/style.css';
+import '../styles/css/bootstrap.css';
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
