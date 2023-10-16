b849b31a91e81d1741ad07dda32f593d015cc003
Royal-13 <ashu.13dun@gmail.com>
2023-10-16 23:05:38 +0530

    images

--- components/home/section2.tsx
+++ components/home/section2.tsx
@@ -17,7 +17,7 @@ function SectionTwo({ }: Props) {
                 opacity:0}}
                 transition={{duration:1.2}}
                 whileInView={{opacity:1,
-                y:0}} loading="lazy" className="rounded-whats-img" srcSet="/img/whats/tiny_v3/mudgar.webp 480w,/img/whats/tiny_v3/mudgar.webp 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/mudgar.webp" />
+                y:0}} loading="lazy" className="rounded-whats-img" srcSet="/img/whats/tiny_v3/mudgar1.jpg 480w,/img/whats/tiny_v3/mudgar1.jpg 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/mudgar1.jpg" />
               <div className="include_details_bg">
                 <h4>COMPLEMENTARY MUDGAR</h4>
                 <div className="include_text">As part of this course, a mudgar weighing 5-6kgs will be provided to men, and a mudgar weighing 3-4kgs will be provided to women, so you don't have to buy it separately.</div>
@@ -31,7 +31,7 @@ function SectionTwo({ }: Props) {
                 opacity:0}}
                 transition={{duration:1.2}}
                 whileInView={{opacity:1,
-                y:0}} loading="lazy"className="rounded-whats-img" srcSet="/img/whats/tiny_v3/45min.webp 480w,/img/whats/tiny_v3/45min.webp 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/45min.webp" />
+                y:0}} loading="lazy"className="rounded-whats-img" srcSet="/img/whats/tiny_v3/live class1.jpg 480w,/img/whats/tiny_v3/live class1.jpg 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/live class1.jpg" />
               <div className="include_details_bg">
                 <h4>45-MINUTE LIVE CLASS</h4>
                 <div className="include_text">For the next three months, a 45-minute live class will be conducted from Monday to Friday.</div>
@@ -45,7 +45,7 @@ function SectionTwo({ }: Props) {
                 opacity:0}}
                 transition={{duration:1.2}}
                 whileInView={{opacity:1,
-                y:0}} loading="lazy" className="rounded-whats-img" srcSet="/img/whats/tiny_v3/3.webp 480w,/img/whats/tiny_v3/3.webp 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/3.webp" />
+                y:0}} loading="lazy" className="rounded-whats-img" srcSet="/img/whats/tiny_v3/recording.jpg 480w,/img/whats/tiny_v3/recording.jpg 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/recording.jpg" />
               <div className="include_details_bg">
                 <h4>RECORDED SESSIONS OF EVERY CLASS</h4>
                 <div className="include_text pb-4">Daily recorded sessions of all classes will be available for the next three months from the start of the course.</div>
@@ -59,7 +59,7 @@ function SectionTwo({ }: Props) {
                 opacity:0}}
                 transition={{duration:1.2}}
                 whileInView={{opacity:1,
-                y:0}} loading="lazy" className="rounded-whats-img" srcSet="/img/whats/tiny_v3/community.webp 480w,/img/whats/tiny_v3/community.webp 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/community.webp" />
+                y:0}} loading="lazy" className="rounded-whats-img" srcSet="/img/whats/tiny_v3/people1.jpg 480w,/img/whats/tiny_v3/people1.jpg 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/people1.jpg" />
               <div className="include_details_bg">
                 <h4>COMMUNITY OF THOUSANDS OF PEOPLE</h4>
                 <div className="include_text">You will have the opportunity to connect and grow with like-minded people and become part of the biggest mudgar club possible for the next three months.</div>
@@ -73,7 +73,7 @@ function SectionTwo({ }: Props) {
                 opacity:0}}
                 transition={{duration:1.2}}
                 whileInView={{opacity:1,
-                y:0}} loading="lazy" className="rounded-whats-img" srcSet="/img/whats/tiny_v3/Maintainance.webp 480w,/img/whats/tiny_v3/Maintainance.webp 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/Maintainance.webp" />
+                y:0}} loading="lazy" className="rounded-whats-img" srcSet="/img/whats/tiny_v3/maintainance1.jpg 480w,/img/whats/tiny_v3/maintainance1.jpg 800w" sizes="(max-width: 600px) 480px, 800px" src="/img/whats/tiny_v3/maintainance1.jpg" />
               <div className="include_details_bg">
                 <h4>MUDGAR MAINTENANCE</h4>
                 <div className="include_text">We will teach you how to take care of your mudgar in the long run since it is made of premium quality Mahaghony Wood.</div>
