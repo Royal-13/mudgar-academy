1e3a907cf3cbf4ab33e2b205826c429b1b3c4560
Karan Kartikeya <karankartikeya@Karans-MacBook-Air.local>
2023-10-16 13:33:53 +0530

    feat: umami and random place data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,12 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
 import { MyUserContextProvider } from "@/utils/useUser";
+import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   return (
     <>
+    <Script src="https://analytics.eu.umami.is/script.js" data-website-id="d4065aa7-8796-459d-8ff1-4e0e4334739f" />
       <SessionContextProvider
         supabaseClient={supabaseClient}
         initialSession={pageProps.initialSession}
